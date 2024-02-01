9f142873aee14b971d0386893251543ea58c9a72
21Kdev <154817638+21Kdev@users.noreply.github.com>
2024-02-02 02:06:10 +0900

    Update install_image_patcher.sh

--- deploy-example/kubernetes-manifest/install_image_patcher.sh
+++ deploy-example/kubernetes-manifest/install_image_patcher.sh
@@ -9,8 +9,8 @@ read -p "Docker Hub Token: " dockerHubToken
 read -p "Docker Hub Email: " dockerHubEmail
 
 if [ -z "$dockerHubId" ] || [ -z "$dockerHubToken" ] || [ -z "$dockerHubEmail" ]; then
-    echo "오류: Docker Hub ID 또는 Token 또는 Email이 누락되었습니다. 작업이 취소되었습니다."
-    exit 1
+  echo "오류: Docker Hub ID 또는 Token 또는 Email이 누락되었습니다. 작업이 취소되었습니다."
+  exit 1
 fi
 
 # 입력 값 확인 및 사용자 승인 요청
@@ -22,54 +22,53 @@ read -p "이 정보로 계속하시겠습니까? [Y/N] " confirmation
 
 if [[ $confirmation =~ ^[Yy]$ ]]
 then
-    # .dockerconfigjson 생성 및 인코딩
-    dockerConfigJson=$(cat <<EOF
-    {
-      "auths": {
-        "https://index.docker.io/v1/": {
-          "username": "$dockerHubId",
-          "password": "$dockerHubToken",
-          "email": "$dockerHubEmail"
-        }
+  # .dockerconfigjson 생성 및 인코딩
+  dockerConfigJson=$(cat <<EOF
+  {
+	  "auths": {
+      "https://index.docker.io/v1/": {
+        "username": "$dockerHubId",
+        "password": "$dockerHubToken",
+        "email": "$dockerHubEmail"
       }
     }
+  }
 EOF
-    )
+  )
 
-	master_node=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane= --output=jsonpath='{.items[0].metadata.name}')
+  master_node=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane= --output=jsonpath='{.items[0].metadata.name}')
 
-	if [ -z "$master_node" ]; then
-	  echo "마스터 노드를 찾을 수 없습니다."
-	  exit 1
-	fi
+  if [ -z "$master_node" ]; then
+    echo "마스터 노드를 찾을 수 없습니다."
+    exit 1
+  fi
 
-	kubectl taint nodes $master_node node-role.kubernetes.io/control-plane:NoSchedule-
+  kubectl taint nodes $master_node node-role.kubernetes.io/control-plane:NoSchedule-
 		
-    encodedDockerConfigJson=$(echo -n "$dockerConfigJson" | base64 -w 0)
-    kubectl apply -f https://raw.githubusercontent.com/21Kdev/imagepullsecret-patcher/master/deploy-example/kubernetes-manifest/0_namespace.yaml
-    kubectl apply -f https://raw.githubusercontent.com/21Kdev/imagepullsecret-patcher/master/deploy-example/kubernetes-manifest/1_rbac.yaml
-    # GitHub Raw 링크로부터 YAML 파일 내용 가져오기, 수정 후 적용
-    curl -s $rawUrl | sed "s/eyJhdXRocyI6eyJnY3IuaW8iOnsicGFzc3dvcmQiOiJ7XCJhdXRoXCI6e1wiZ2NyLmlvXCI6e1widXNlcm5hbWVcIjpcIl9qc29uX2tleVwiLFwicGFzc3dvcmRcIjpcInt9XCJ9fX0iLCJ1c2VybmFtZSI6Il9qc29uX2tleSJ9fX0=/$encodedDockerConfigJson/" | kubectl apply -f -
+  encodedDockerConfigJson=$(echo -n "$dockerConfigJson" | base64 -w 0)
+  kubectl apply -f https://raw.githubusercontent.com/21Kdev/imagepullsecret-patcher/master/deploy-example/kubernetes-manifest/0_namespace.yaml
+  kubectl apply -f https://raw.githubusercontent.com/21Kdev/imagepullsecret-patcher/master/deploy-example/kubernetes-manifest/1_rbac.yaml
+  # GitHub Raw 링크로부터 YAML 파일 내용 가져오기, 수정 후 적용
+  curl -s $rawUrl | sed "s/eyJhdXRocyI6eyJnY3IuaW8iOnsicGFzc3dvcmQiOiJ7XCJhdXRoXCI6e1wiZ2NyLmlvXCI6e1widXNlcm5hbWVcIjpcIl9qc29uX2tleVwiLFwicGFzc3dvcmRcIjpcInt9XCJ9fX0iLCJ1c2VybmFtZSI6Il9qc29uX2tleSJ9fX0=/$encodedDockerConfigJson/" | kubectl apply -f -
 		
-	# Deployment의 파드가 1/1이 되기를 기다립니다.
-	echo -n "imagepullsecret-patcher pod 생성을 기다리는 중입니다"
-	while true; do
-		echo -ne "..."
-	  desired=$(kubectl get deployment imagepullsecret-patcher -n imagepullsecret-patcher --output=jsonpath='{.spec.replicas}')
-	  current=$(kubectl get deployment imagepullsecret-patcher -n imagepullsecret-patcher --output=jsonpath='{.status.replicas}')
-	  if [ "$desired" -eq "$current" ]; then
-		break
-	  fi
-	  sleep 0.5
-		
-		echo -ne "\b\b\b"
-	done
-	sleep 1
-	echo ""
- 	echo ""
-	kubectl taint nodes $master_node node-role.kubernetes.io/control-plane:NoSchedule
-	kubectl patch deployment imagepullsecret-patcher -n imagepullsecret-patcher --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/hostNetwork", "value": false}]'
-	kubectl patch deployment imagepullsecret-patcher -n imagepullsecret-patcher --type='json' -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
+  # Deployment의 파드가 1/1이 되기를 기다립니다.
+  echo -n "imagepullsecret-patcher pod 생성을 기다리는 중입니다"
+  while true; do
+    echo -ne "..."
+    desired=$(kubectl get deployment imagepullsecret-patcher -n imagepullsecret-patcher --output=jsonpath='{.spec.replicas}')
+    current=$(kubectl get deployment imagepullsecret-patcher -n imagepullsecret-patcher --output=jsonpath='{.status.replicas}')
+    if [ "$desired" -eq "$current" ]; then
+      break
+    fi
+    sleep 0.5	
+    echo -ne "\b\b\b"
+  done
+  sleep 1
+  echo ""
+  echo ""
+  kubectl taint nodes $master_node node-role.kubernetes.io/control-plane:NoSchedule
+  kubectl patch deployment imagepullsecret-patcher -n imagepullsecret-patcher --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/hostNetwork", "value": false}]'
+  kubectl patch deployment imagepullsecret-patcher -n imagepullsecret-patcher --type='json' -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
 else
-    echo "작업이 취소되었습니다."
+  echo "작업이 취소되었습니다."
 fi
